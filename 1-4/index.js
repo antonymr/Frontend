36978de6860b807ac30193fde036c5812e075e3f
Antony Macias <68290460+antonymr@users.noreply.github.com>
2022-06-06 12:58:36 -0500

    Update selection

--- 1-4/index.js
+++ 1-4/index.js
@@ -1,55 +1,55 @@
 window.addEventListener("DOMContentLoaded", function (event){
     let form = document.getElementById("form_register")
     form.addEventListener("submit", function(event){
-        event.preventDefault();
+        //event.preventDefault();
         let cant_submit = true;
+        //Name
         var input_name = document.querySelector("#input_name");
         if (empty_validate(input_name, "#name_container", "EL nombre")){
             if (input_name.value.length < 3){
-                document.querySelector("#name_container .input_error").innerHTML = 'El nombre debe tener minimo 3 caracteres'
-                cant_submit = false
+                document.querySelector("#name_container .input_error").innerHTML = 'El nombre debe tener minimo 3 caracteres';
+                cant_submit = false;
             }
         }else{
-            cant_submit = false
+            cant_submit = false;
         }
-
+        //Last Name
         var input_last_name = document.querySelector("#input_last_name");
         if (empty_validate(input_last_name, "#last_name_container", "El apellido")){
             if (input_last_name.value.length < 3){
-                document.querySelector("#last_name_container .input_error").innerHTML = 'El apellido debe tener minimo 3 caracteres'
-                cant_submit = false
+                document.querySelector("#last_name_container .input_error").innerHTML = 'El apellido debe tener minimo 3 caracteres';
+                cant_submit = false;
             }
         }else{
-            cant_submit = false
+            cant_submit = false;
         }
-
+        //Identifier
         var input_identifier_type = document.querySelector("#identifier_type");
         var input_identifier = document.querySelector("#input_identifier");
         if (empty_validate(input_identifier, "#identifier_container", "La cedula")){
             if ((input_identifier.value.length != 10 || isNaN(input_identifier.value)) && input_identifier_type.value == 'ci'){
-                document.querySelector("#identifier_container .input_error").innerHTML = 'La cedula debe tener 10 caracteres numericos'
-                cant_submit = false
+                document.querySelector("#identifier_container .input_error").innerHTML = 'La cedula debe tener 10 caracteres numericos';
+                cant_submit = false;
             }
             if ((input_identifier.value.length != 13 || isNaN(input_identifier.value)) && input_identifier_type.value == 'ruc'){
-                document.querySelector("#identifier_container .input_error").innerHTML = 'El RUC debe tener 13 caracteres numericos'
-                cant_submit = false
+                document.querySelector("#identifier_container .input_error").innerHTML = 'El RUC debe tener 13 caracteres numericos';
+                cant_submit = false;
             }
         }else{
-            cant_submit = false
+            cant_submit = false;
         }
-
+        //Email
         var input_email = document.querySelector("#input_email");
         if (empty_validate(input_email, "#email_container", "El correo")){
             var index_one = input_email.value.indexOf('@');
             var index_two = input_email.value.indexOf('.');
             if ((index_one < 0 || index_two < 0) || index_one > index_two ){
-                document.querySelector("#email_container .input_error").innerHTML = 'Correo no tiene un formato valido'
-                cant_submit = false
+                document.querySelector("#email_container .input_error").innerHTML = 'Correo no tiene un formato valido';
+                cant_submit = false;
             }
         }else{
-            cant_submit = false
+            cant_submit = false;
         }
-
         /*var input_date = document.querySelector("#input_start_date");
         if (input_date.value == ""){
             cant_submit = false
@@ -62,10 +62,8 @@ window.addEventListener("DOMContentLoaded", function (event){
             }
             document.querySelector("#start_date_container .input_error").innerHTML = ''
         }*/
-
-
         if (!cant_submit){
-            event.preventDefault()
+            event.preventDefault();
         }
     })
 })
